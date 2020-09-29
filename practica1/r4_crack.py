17359632b02a979bacc72f54260751515cb1a72b
Aleix <algafix@gmail.com>
2020-09-29 03:11:51 +0200

    console log formatting

--- practica1/r4_crack.py
+++ practica1/r4_crack.py
@@ -1,11 +1,14 @@
 import csv 
 
+# Original values
+
 o_m0 = 'a'
 o_key = '000102030405060708090a0b0c'
 
+print()
 print('message is: ' + o_m0)
 print('key is: ' + o_key)
-print('=============================')
+print('\n=============================')
 
 ############# Guessing m0 #############
 
@@ -41,12 +44,14 @@ with open('data_k.txt', mode='r') as csv_file:
     for row in csv_reader:
 
         if row['IV'] == '0'+format(iv_counter, 'x')+'ff00':
-            # Set list to 0 for new k
+            # First value of this k
+            # Set the frequence list to 0
             k_freq_list = []
             for k_value in range(0,255):
                 k_freq_list.append(0)
 
         for k_value in range(0,255):
+            # Computes the guessing for this IV value
             keystream_value = m0 ^ int('0x'+row['Ciphertext'],0)
             suposed_value = (int('0x'+row['IV'][-2:],0) + guess_counter + k_value) % 255
             if keystream_value == suposed_value:
@@ -54,19 +59,25 @@ with open('data_k.txt', mode='r') as csv_file:
         
         if row['IV'] == '0'+format(iv_counter, 'x')+'ffff':
             # Last value of this k
+            # Saves and compares the guessed k
             frequency = max(k_freq_list)
             k = k_freq_list.index(frequency)
             recovered_key.append(k)
             verification = k == int('0x'+o_key[k_byte*2:k_byte*2+2],0)
             print('k['+str(k_byte)+']: ' + "{0:#0{1}x}".format(k,4) + '\t(with freq. ' + str(frequency) + ')\t\t' + str(verification))
 
+            # Update the counters for next k guessing
             iv_counter += 1
             guess_counter += iv_counter + k
             k_byte += 1
             
+# Console log of the key recovered
+
+print('=============================\n')
 recovered_key_string = ''.join(["{0:0{1}x}".format(x,2) for x in recovered_key])
 if o_key == recovered_key_string:
     print('\033[32m Key recovered! \033[m\n\t' + o_key + ' == ' + recovered_key_string)
 else:
     print('\033[31m Key not recovered \033[m\n\t' + o_key + ' != ' + recovered_key_string)
 
+print()
