0f6783e5293a047faf6e8bd59a3c8cf2442d087c
Aleix <algafix@gmail.com>
2020-10-20 17:53:59 +0200

    documentation

--- practica2/exercice_2/add_node.sh
+++ practica2/exercice_2/add_node.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
 IFS=":";
 read -r name alg doc_pre node_pre n max_i root < merkle_tree.txt
 
+# New document computation
 i=0
 cat docs/doc.pre docs/doc$n.dat | openssl dgst -sha1 -binary > nodes/node$i.$n
 sed -i "/^$i:$(($n-1)):/a $i:$n:$(cat nodes/node$i.$n | xxd -p)" merkle_tree.txt
 
+# Updating the tree
 j=$n
-
 while [ $j -gt 0 ]
 do
 
@@ -17,11 +17,11 @@ i=$(($i+1))
 odd=$(($j%2))
 j=$(($j/2))
 
-if [ $odd -eq 1 ]
+if [ $odd -eq 1 ] # If it has no right node
 then
-cat nodes/node.pre nodes/node$(($i-1)).$((2*$j)) nodes/node$(($i-1)).$((2*$j+1)) | openssl dgst -sha1 -binary > nodes/node$i.$j;
+    cat nodes/node.pre nodes/node$(($i-1)).$((2*$j)) nodes/node$(($i-1)).$((2*$j+1)) | openssl dgst -sha1 -binary > nodes/node$i.$j;
 else
-cat nodes/node.pre nodes/node$(($i-1)).$((2*$j)) | openssl dgst -sha1 -binary > nodes/node$i.$j;
+    cat nodes/node.pre nodes/node$(($i-1)).$((2*$j)) | openssl dgst -sha1 -binary > nodes/node$i.$j;
 fi;
 sed -i "s/^$i:$j:.*/$i:$j:$(cat nodes/node$i.$j | xxd -p)/g" merkle_tree.txt
 done;
--- practica2/exercice_2/create_merkle.sh
+++ practica2/exercice_2/create_merkle.sh
@@ -39,27 +39,3 @@ done;
 done;
 
 echo -e "MerkleTree:sha1:${doc_prepend:2}:${node_prepend:2}:$n:$(($i+1)):$(cat nodes/node$i.$j | xxd -p)\n$(cat merkle_tree.txt)" > merkle_tree.txt
-
-## OLD
-
-# j_float=$(echo "l($n)/l(2)" | bc -l)
-# j_float=${j_float%?}
-# j=$(echo "scale = 0; $j_float/1" | bc -l)
-
-# if echo "$j_float > $j" | bc -l | grep -q 1
-# then
-#     j=$(($j+1))
-# fi
-
-# echo $j
-
-# max_n_f=$(echo "l($n)/l(2)" | bc -l)
-# max_n_f=${max_n_f%?}
-# max_n=$(echo "scale = 0; $max_n_f/1" | bc -l)
-
-# if echo "$max_n_f > $max_n" | bc -l | grep -q 1
-# then
-#     max_n=$(($max_n+1))
-# fi
-
-# max_n=$((2**$max_n))
