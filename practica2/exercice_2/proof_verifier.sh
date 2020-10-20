6196567c46ef535652fd2284abd72c7ea54605d9
Aleix <algafix@gmail.com>
2020-10-20 19:50:49 +0200

    proof_verified fixed!

--- practica2/exercice_2/proof_verifier.sh
+++ practica2/exercice_2/proof_verifier.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-doc_k=$1
+writehex  ()
+{
+    local i
+    while [ "$1" ]; do
+        for ((i=0; i<${#1}; i+=2))
+        do
+            printf "\x${1:i:2}";
+        done;
+        shift;
+    done
+}
 
+
+# Hash of the document (leaf)
+doc_k=$1
 IFS=":";
 read -r name alg doc_pre node_pre n max_i root < $2;
 
@@ -10,7 +23,6 @@ hash=$(echo -n -e "\x${doc_pre}${doc_content}" | openssl dgst -sha1 -binary | xx
 
 j=$1
 i=0
-max_j=$n
 
 {
 read
@@ -18,19 +30,12 @@ while IFS=":"; read v_i v_j node_hash;
 do
     if [ $((j%2)) -eq 0 ]
     then
-        if [ $(($j+1)) -lt $max_j ]
-        then
-            hash=$(echo -n $(echo -n -e "\x${node_pre}")$(echo -n "${hash}${node_hash}" | xxd -r -p) | openssl dgst -sha1 -binary | xxd -p)
-        else
-            hash=$(echo -n $(echo -n -e "\x${node_pre}")$(echo -n "${hash}" | xxd -r -p) | openssl dgst -sha1 -binary | xxd -p)
-        fi
+        hash=$(writehex ${node_pre}${hash}${node_hash} | openssl dgst -sha1 -binary | xxd -p)
     else
-        hash=$(echo -n $(echo -n -e "\x${node_pre}")$(echo -n "${node_hash}${hash}" | xxd -r -p) | openssl dgst -sha1 -binary | xxd -p)
+        hash=$(writehex ${node_pre}${node_hash}${hash} | openssl dgst -sha1 -binary | xxd -p)
     fi
-
     j=$(($j/2))
     i=$(($i+1))
-    max_j=$(($max_j/2+$max_j%2))
 done;
 } < $2
 
