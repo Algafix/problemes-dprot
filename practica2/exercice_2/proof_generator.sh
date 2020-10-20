14a370b050a67115dc5ae3c57605ca374bfb7b74
Aleix <algafix@gmail.com>
2020-10-20 18:23:52 +0200

    patched missing nodes in the proof genertion

--- practica2/exercice_2/proof_generator.sh
+++ practica2/exercice_2/proof_generator.sh
@@ -10,13 +10,15 @@ max_j=$n
 
 echo "$name:$alg:$doc_pre:$node_pre:$n:$max_i:$root" > proof_doc$doc_k.txt
 
-while [ $i -lt $max_i ]
+while [ $i -lt $(($max_i-1)) ]
 do
     if [ $((j%2)) -eq 0 ]
     then
         if [ $(($j+1)) -lt $max_j ]
         then
             echo "$i:$(($j+1)):$(cat nodes/node$i.$(($j+1)) | xxd -p)" >> proof_doc$doc_k.txt
+        else
+            echo "$i:$(($j+1)):" >> proof_doc$doc_k.txt
         fi
     else
         echo "$i:$(($j-1)):$(cat nodes/node$i.$(($j-1)) | xxd -p)" >> proof_doc$doc_k.txt
